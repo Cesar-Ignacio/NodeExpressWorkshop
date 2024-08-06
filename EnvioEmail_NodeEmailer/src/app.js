81955731def1327d2edf16c9aa0ff705e51b3d9e
cesar Tello <aquinocesarignacio@gmail.com>
2024-08-06 10:11:48 -0300

    Nodemailer

--- EnvioEmail_NodeEmailer/src/app.js
+++ EnvioEmail_NodeEmailer/src/app.js
@@ -1,13 +1,48 @@
 import express from 'express'
 import { config } from './config.js';
+import nodeemailer from 'nodemailer';
 
+const app = express();
 
-const app=express();
+console.log(config.GMAIL_APP_USER);
+console.log(config.PASS_APP_GMAIL);
 
-app.get("/",(req,res)=>{
+const transport = nodeemailer.createTransport({
+    service: 'gmail',
+    port: 587,
+    auth: {
+        user: config.GMAIL_APP_USER,
+        pass: config.PASS_APP_GMAIL
+    }
+})
+
+app.get("/", (req, res) => {
     res.status(200).send("<h1>Bienvenido!</h1>")
 });
 
-app.listen(config.PORT,()=>{
+app.get('/mail', async (req,res) => {
+
+    try {
+        let result = await transport.sendMail({
+            from: `E-commerce <${config.GMAIL_APP_USER}>`,
+            to: "portfolio1a4@gmail.com",
+            subject: "Prueba de NodeEmailer",
+            html: `
+                <div>
+                    <strong>Este es un mensaje de prueba 2</strong>
+                </div>
+            `
+        })
+        console.log(result)
+        res.status(200).send(result)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+    }
+
+
+})
+
+app.listen(config.PORT, () => {
     console.log(`Servidor activo en port ${config.PORT}`)
 })
--- EnvioEmail_NodeEmailer/src/config.js
+++ EnvioEmail_NodeEmailer/src/config.js
@@ -4,5 +4,6 @@ dotenv.config();
 
 export const config={
     PORT:8080,
+    GMAIL_APP_USER:process.env.GMAIL_APP_USER,
     PASS_APP_GMAIL:process.env.PASS_APP_GMAIL
 }
